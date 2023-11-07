5a39cb5a9a90f045ec6326c33311ffd07ad4c9a5
liulanzheng <lanzheng.liulz@alibaba-inc.com>
2023-11-07 16:33:25 +0800

    update photon to v0.7.1
    Signed-off-by: liulanzheng <lanzheng.liulz@alibaba-inc.com>

--- CMake/Findphoton.cmake
+++ CMake/Findphoton.cmake
@@ -4,7 +4,7 @@ set(FETCHCONTENT_QUIET false)
 FetchContent_Declare(
   photon
   GIT_REPOSITORY https://github.com/alibaba/PhotonLibOS.git
-  GIT_TAG v0.6.1
+  GIT_TAG v0.7.1
 )
 FetchContent_MakeAvailable(photon)
 set(PHOTON_INCLUDE_DIR ${photon_SOURCE_DIR}/include/)
